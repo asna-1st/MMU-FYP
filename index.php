2005ff933b39755085fca348e9d13d72206c39ec
Nafiz <nalnul109@gmail.com>
2021-01-05 22:26:40 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <form>
+        <div class="mb-3">
+            <label>Email</label><br>
+            <input class="form-control" type="email" id="email"></input>
+        </div>
+        <br>
+        <label>Password</label><br>
+        <input class="form-control" type="password" id="pass"></input>
+    </form>
+</body>
+
+</html>
