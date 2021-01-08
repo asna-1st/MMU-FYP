714d92bfff03ebf82fc57b61f8581b53a5d9b3ea
Nafiz <nalnul109@gmail.com>
2021-01-08 17:07:40 +0800

    adding main page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+
+    <body>
+        <section>
+            
+        </section>
+    </body>
+</html>
